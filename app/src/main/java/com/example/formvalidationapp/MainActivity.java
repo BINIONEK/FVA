0b0f3c29d0aebb621eabdd511528aa143e058bad
Your Name <you@example.com>
2024-09-30 08:59:16 +0200

    binded

--- app/src/main/java/com/example/formvalidationapp/MainActivity.java
+++ app/src/main/java/com/example/formvalidationapp/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.formvalidationapp;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextName;
+    private EditText editTextSurname;
+    private EditText editTextEmailAddress;
+    private EditText editTextPhoneNumber;
+    private EditText editTextPassword;
+    private EditText editTextPasswordConfirmation;
+    private Button buttonSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextName = findViewById(R.id.editTextName);
+        editTextSurname = findViewById(R.id.editTextSurname);
+        editTextEmailAddress = findViewById(R.id.editEmailAddress);
+        editTextPhoneNumber = findViewById(R.id.editTextPhoneNumber);
+        editTextPassword = findViewById(R.id.editTextPasssword);
+        editTextPasswordConfirmation = findViewById(R.id.editTextPassswordConfirmation);
+        buttonSubmit = findViewById(R.id.buttonSubmit);
     }
 }
